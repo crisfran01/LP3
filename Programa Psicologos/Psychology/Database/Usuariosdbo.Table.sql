eea15bfe6c5482c991e3b0a19171034ad647cd09
Cristiane Franchini <crisfr01@gmail.com>
2018-05-14 08:16:18 -0300

    remove things and write readme

--- Programa Psicologos/Psychology/Database/Usuariosdbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Usuarios]
-(
-	[cod] INT NOT NULL PRIMARY KEY, 
-    [Nome] NCHAR(20) NULL, 
-    [senha] NCHAR(10) NULL, 
-    [nivel] INT NULL
-)
--- Programa Psicologos/Psychology/Database/dbo.Usuarios.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Usuarios] (
-    [cod]   INT        NOT NULL IDENTITY,
-    [Nome]  NCHAR (20) NULL,
-    [senha] NCHAR (10) NULL,
-    [nivel] INT        NULL,
-    PRIMARY KEY CLUSTERED ([cod] ASC)
-);
-
